4e1b1b453f2662ec0a6bafb2a6e3b5051f7c86f4
yardtekle022 <yaredtekle22@gmail.com>
2023-04-04 11:40:04 +0300

    fix linter

--- modules/time.js
+++ modules/time.js
@@ -1,5 +1,5 @@
 // timer
-import { DateTime } from '/node_modules/luxon/src/luxon.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const displayDate = () => {
   document.querySelector('.current-date').innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
